3ed293c34f5e482685ed548d38fb3147ab525fdd
Jessexd <Jessexd@users.noreply.github.com>
2018-02-13 00:04:27 -0500

    tons of shit

--- plugins/speccy.py
+++ plugins/speccy.py
@@ -12,14 +12,12 @@ from bs4 import BeautifulSoup as BS
 def get_speccy_url(conn, message, chan, content, nick):
     re_content = re.search(r"https?:\/\/speccy.piriform.com\/results\/[A-z0-9]+", content)
     if re_content:
-        # message("({}) Analyzing Speccy URL...".format(nick))
         return parse_speccy(message, nick, str(re_content.group(0)))
 
 def parse_speccy(message, nick, url):
 
     response = requests.get(url)
     if not response:
-        #return "Error retrieving speccy URL"
         return None
 
     soup = BS(response.content, "lxml-xml")
@@ -40,37 +38,34 @@ def parse_speccy(message, nick, url):
         cpuspec = None
 
     try:
-        gpuspec = soup.body.find("div", text='Graphics').next_sibling.next_sibling.stripped_strings
-        graphics_list = []
-        for item in gpuspec:
-            graphics_list.append(item)
-        length = len(graphics_list)
-        gpuspec = graphics_list[length-2] + " " + graphics_list[length-1]
-    except Exception:
+        gpufind = soup.body.find("div", text='Graphics').next_sibling.next_sibling.text
+        for gpuspec in re.finditer(r".*(amd|radeon|intel|integrated|nvidia|geforce|gtx).*\n.*", gpufind, re.I):
+            print(gpuspec.group(0)) # <--------- this is proof it works, but I can't make the bot output it right
+    except AttributeError:
         gpuspec = None
 
     try:
-        picospec = soup.body.find("div", text=re.compile('.*pico', re.IGNORECASE)).text
+        picospec = soup.body.find("div", text=re.compile('.*pico', re.I)).text
     except AttributeError:
         picospec = None
 
     try:
-        kmsspec = soup.body.find("div", text=re.compile('.*kms', re.IGNORECASE)).text
+        kmsspec = soup.body.find("div", text=re.compile('.*kms', re.I)).text
     except AttributeError:
         kmsspec = None
 
     try:
-        boosterspec = soup.body.find("div", text=re.compile('.*booster', re.IGNORECASE)).text
+        boosterspec = soup.body.find("div", text=re.compile('.*booster', re.I)).text
     except AttributeError:
         boosterspec = None
 
     try:
-        xtuspec = soup.body.find("div", text=re.compile('.*xtu', re.IGNORECASE)).text
+        xtuspec = soup.body.find("div", text=re.compile('.*xtu', re.I)).text
     except AttributeError:
         xtuspec = None
 
     try:
-        reviverspec = soup.body.find("div", text=re.compile('.*reviver', re.IGNORECASE)).text
+        reviverspec = soup.body.find("div", text=re.compile('.*reviver', re.I)).text
     except AttributeError:
         reviverspec = None
 
@@ -91,7 +86,7 @@ def parse_speccy(message, nick, url):
     try:
         z = smartcheck()
         if len(z) != 0:
-            smartspec = ""
+            smartspec = " Disk:"
             for item in z:
                 smartspec += " #" + item + " "
         else:
@@ -99,21 +94,13 @@ def parse_speccy(message, nick, url):
     except Exception:
         smartspec = None
 
-    piracy_list = [picospec, kmsspec]
-    piracy = ', '.join(filter(None, piracy_list))
-    if not piracy:
-        piracy = None
-
-    badware_list = [boosterspec, xtuspec, reviverspec]
+    badware_list = [picospec, kmsspec, boosterspec, xtuspec, reviverspec]
     badware = ', '.join(filter(None, badware_list))
     if not badware:
         badware = None
 
-    specin = "\x02OS:\x02 {} ● \x02RAM:\x02 {} ● \x02CPU:\x02 {} ● \x02GPU:\x02 {} ● \x02Piracy:\x02 {} ● \x02Badware:\x02 {} ● \x02Failing Drive(s):\x02 {}".format(osspec, ramspec, cpuspec, gpuspec, piracy, badware, smartspec)
+    specin = "\x02OS:\x02 {} ● \x02RAM:\x02 {} ● \x02CPU:\x02 {} ● \x02GPU:\x02 {} ● \x02Badware:\x02 {} ● \x02Failing Drive(s):\x02 {}".format(osspec, ramspec, cpuspec, gpuspec, badware, smartspec)
 
     specout = re.sub("\s{2,}|\r\n|\n", " ", specin)
-
-    if piracy:
-        message("({}) WARNING: Piracy sofware found ({}). Please be advised we do not support users running software that violates the Terms of Service.".format(nick, piracy))
 
     return specout
